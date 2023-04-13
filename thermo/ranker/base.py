5452d51c1edeb204f6152faab062be5004da4e5b
Estefania Garijo <estefania.rio@bs.nttdata.com>
2023-04-13 10:46:49 +0200

    Better typing hints

--- thermo/ranker/base.py
+++ thermo/ranker/base.py
@@ -1,11 +1,14 @@
 from abc import ABC, abstractmethod
 from typing import Any
 
+from costs import CostModel
+from numpy.typing import NDArray
+
 
 class Ranker(ABC):
-    def __init__(self, costs: list, config: dict[str, Any]):
+    def __init__(self, costs: list[CostModel], config: dict[str, Any]):
         self.costs = costs
 
     @abstractmethod
-    def run(self, schedule: Any) -> list:
+    def run(self, schedule: NDArray) -> list:
         pass
