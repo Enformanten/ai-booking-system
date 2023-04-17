1f4d12154187f74a7fbc706803c6b25e84fdbcf0
Estefania Garijo <estefania.rio@bs.nttdata.com>
2023-04-17 13:22:11 +0200

    Add tests for loaders of files for each subscription

--- tests/unit_tests/test_utils/test_io.py
+++ tests/unit_tests/test_utils/test_io.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pytest
 
-from thermo.utils.io import load_adjacency
+from thermo.utils.io import load_adjacency, load_config, load_room_description
 
 
 class TestSubscription:
@@ -10,8 +10,16 @@ class TestSubscription:
         self.path = tmp_path
         # TODO: Save the adjacency for graph to a file in self.path
         # TODO: Save a config to a file in self.path
+        # TODO: Save a room description to file in self.path
         yield
 
     def test_load_adjacency(self, graph):
         adjacency = load_adjacency(self.path)
         assert np.all(adjacency == graph)
+
+    def test_load_config(self, config):
+        configuration = load_config(self.path)
+        assert config == configuration
+
+    def test_load_room(self, room_description):
+        assert room_description == load_room_description(self.path)
