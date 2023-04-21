acf4c1c8544d2913c81b32355d9b5e7aeb71f4a2
Estefania Garijo <estefania.rio@bs.nttdata.com>
2023-04-21 13:52:58 +0200

    Add support for CapacityCost in make_cost

--- thermo/costs/__init__.py
+++ thermo/costs/__init__.py
@@ -1,14 +1,17 @@
 from typing import Literal
 
-from thermo.costs.base import CostModel  # noqa
+from thermo.costs.base import CostModel
+from thermo.costs.capacity import CapacityCost
 from thermo.costs.heating import HeatingCost
 
-CostName = Literal["HeatingCost"]
+CostName = Literal["HeatingCost", "CapacityCost"]
 
 
-def make_cost(name: CostName, **kwargs):
+def make_cost(name: CostName, **kwargs) -> CostModel:
     match name:
         case "HeatingCost":
             return HeatingCost(**kwargs)
+        case "CapacityCost":
+            return CapacityCost(**kwargs)
         case _:
             raise NotImplementedError(f"CostModel {name} not implemented.")
