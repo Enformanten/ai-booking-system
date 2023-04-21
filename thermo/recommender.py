fdea790b270c45659999a944662da6f0052cce18
Estefania Garijo <estefania.rio@bs.nttdata.com>
2023-04-21 11:07:21 +0200

    Add documentation

--- thermo/recommender.py
+++ thermo/recommender.py
@@ -42,6 +42,21 @@ class Recommender:
         room_description: list[Room],
         ranker: Ranker,
     ):
+        """
+        Init method, intended for testing purposes and not for human
+        use. We recommend instantiating recommenders from config files
+        by calling Recommender.from_config(school_name).
+
+        Args:
+            school_name: Name of the school, as in the path to its config
+                files.
+            room_description: List of rooms containing their characteristics
+                such as name, capacity or index.
+            ranker: Object to orchestrate the different costs and how they are
+                combined. Examples of costs are `thermo.costs.HeatingCost` or
+                `thermo.costs.CapacityCost`.
+        """
+
         self.school_name = school_name
         self.room_description = room_description
         self._room_names = [room.name for room in room_description]
@@ -49,6 +64,17 @@ class Recommender:
 
     @classmethod
     def from_config(cls, school_name: str) -> "Recommender":
+        """
+        Creates a Recommender from the config files of a school.
+
+        Args:
+            school_name: Name of the school, as in the path to its config
+                files.
+
+        Returns:
+            A recommender based on the configuration for that school found in
+            `schools/school_name`.
+        """
         school_path = WORKDIR / "schools" / school_name
         if school_name != "demo_school":
             if not school_path.exists():
@@ -75,6 +101,22 @@ class Recommender:
         )
 
     def run(self, day: date, **kwargs) -> Recommendation:
+        """
+        Produces a recommendation of which rooms to book given a date.
+        The method calls internally the schools API to determine which
+        books have been already booked.
+
+        It then calls all the costs and combines them according to the
+        ranker, to return an instance of Recommendation, with all
+        the costs for the rooms.
+
+        Args:
+            day: date for which the user desires make a booking.
+            kwargs: other possible run-time parameters for the costs.
+
+        Returns:
+            The costs of the recommended possible bookings.
+        """
         state = get_state(day)
         n_time_slots = get_time_slots(day)
         recommendation = self.ranker.run(state, n_time_slots=n_time_slots, **kwargs)
