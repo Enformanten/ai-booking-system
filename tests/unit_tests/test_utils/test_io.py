cdf71c1ba39be22c3cdf994d73263e149ce8609a
nbt <nicolai.thomsen@bs.nttdata.com>
2023-04-21 14:56:55 +0200

    fully integrates config file in tests and solution

--- tests/unit_tests/test_utils/test_io.py
+++ tests/unit_tests/test_utils/test_io.py
@@ -1,25 +1,49 @@
+from pathlib import Path
+
 import numpy as np
 import pytest
+from numpy.typing import NDArray
+from scipy.linalg import issymmetric
 
-from thermo.utils.io import load_adjacency, load_config, load_room_description
+from thermo.utils import io
+from thermo.utils.building import Building
 
 
-class TestSubscription:
+class TestBuilding:
     @pytest.fixture(autouse=True)
-    def subscription_path(self, tmp_path):
-        self.path = tmp_path
-        # TODO: Save the adjacency for graph to a file in self.path
-        # TODO: Save a config to a file in self.path
-        # TODO: Save a room description to file in self.path
+    def load_building_config(self, tmp_path) -> None:
         yield
 
-    def test_load_adjacency(self, graph):
-        adjacency = load_adjacency(self.path)
-        assert np.all(adjacency == graph)
+    def test_config_dont_exist(self) -> None:
+        """Tests that an error is raised if the building
+        does not exist"""
+        with pytest.raises(FileExistsError):
+            _ = io.get_building_path("non_existent_building")
 
-    def test_load_config(self, config):
-        configuration = load_config(self.path)
-        assert config == configuration
+    def test_config_exists(self, demo_building_name: str) -> None:
+        """Tests that the config exists for the given building."""
+        building_path = io.get_building_path(demo_building_name)
+        assert isinstance(building_path, Path)
 
-    def test_load_room(self, room_description):
-        assert room_description == load_room_description(self.path)
+    def test_building_specs(
+        self, core_building_specs: set, all_buildings: list[Building]
+    ) -> None:
+        """Tests that the correct properties, in the correct
+        formats, are present in all building specs.
+        args:
+           core_building_specs: list of properties that must
+               be present in all building specs.
+           all_buildings: list of all building dirs found in
+               the buildings directory.
+        """
+        for building in all_buildings:
+            assert core_building_specs <= building.__dict__.keys()
+            assert issymmetric(building.adjacency)
+            assert len(building.room_descriptions) == building.adjacency.shape[0]
+
+    def test_load_adjacency(
+        self, demo_building_specs: set, demo_graph: NDArray
+    ) -> None:
+        """Tests that the loaded adjacency matrix is the same as the
+        static demo adjacency matrix."""
+        assert np.allclose(demo_building_specs.adjacency, demo_graph)
--- thermo/utils/io.py
+++ thermo/utils/io.py
@@ -1,13 +1,9 @@
 from pathlib import Path
-from typing import Any
 
-import numpy as np
 import yaml
-from numpy.typing import NDArray
 
-from thermo.config import WORKDIR
+from thermo.config import BUILDINGS_DIR
 from thermo.utils.building import Building
-from thermo.utils.room import Room
 
 
 def get_building_path(building_name: str) -> Path:
@@ -22,89 +18,106 @@ def get_building_path(building_name: str) -> Path:
     Returns:
         pathlib.Path: path to the building config dir.
     """
-    building_path = WORKDIR / "buildings" / building_name
+    building_path = BUILDINGS_DIR / building_name
     if not building_path.exists():
-        raise FileExistsError(f"Bulding '{building_name}' does not exist.")
+        raise FileExistsError(f"Building '{building_name}' does not exist.")
     return building_path
 
 
-def get_building_specs(building_path: str):
-    """Loads specifications from building config dir."""
+def get_building_specs(building_path: str) -> Building:
+    """Loads specifications from building config dir.
+    and loads them into a Building object."""
 
     with open(building_path / "config.yaml", "r") as f:
         data = yaml.safe_load(f)
     return Building(**data)
 
 
-def load_adjacency(path: Path) -> NDArray:
+def get_all_buildings() -> list[Building]:
     """
-    Loads adjacency matrix from file.
-    Args:
-        path: path to the subscription
-            where the adjacency matrix is.
-
+    Returns a list of all buildings found in the
+    BUILDINGS_DIR as Building objects.
     Returns:
-        a (N_rooms, N_rooms) matrix where 0 represents
-        two rooms not sharing a wall and 1 represents
-        said rooms sharing a wall.
+        list[Building]: list of all buildings.
     """
-    # TODO: This is a placeholder, the correct implementation
-    # TODO: must be added here. :)
-
-    return np.array(
-        [
-            [0, 1, 0, 0, 0, 0, 0, 0, 0, 1],
-            [1, 0, 0, 1, 0, 1, 1, 0, 0, 0],
-            [0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
-            [0, 1, 1, 0, 0, 0, 1, 0, 0, 0],
-            [0, 0, 0, 0, 0, 1, 1, 1, 0, 0],
-            [0, 1, 0, 0, 1, 0, 0, 0, 0, 0],
-            [0, 1, 0, 1, 1, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 1, 0, 0, 0, 1, 0],
-            [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
-            [1, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-        ]
-    )
-
-
-def load_config(path: Path) -> dict[str, Any]:
-    """
-    Loads school-specific configuration from a file.
-    Args:
-        path: path to the subscription
-            where the adjacency matrix is.
-
-    Returns:
-        The configuration defined by the user.
-    """
-    # TODO: This is a placeholder, the correct implementation
-    # TODO: must be added here. :)
-    return {
-        "ranker": "FullRanker",
-        "costs": {"HeatingCost": {"t_weight": 1.0, "message_importance": 0.5}},
-    }
-
-
-def load_room_description(path: Path) -> list[Room]:
-    """
-    Loads a description of each room from a file.
-    Args:
-        path: path to the subscription where the adjacency matrix is.
-    Returns:
-        a list of rooms with the same indexing as the adjacency.
-    """
-    # TODO: This is a placeholder, the correct implementation
-    # TODO: must be added here. :)
-    room_descriptions = [
-        {"name": "Room A", "capacity": 30},
-        {"name": "Room B", "capacity": 20},
-        {"name": "Room C", "capacity": 10},
-        {"name": "Room D", "capacity": 30},
-        {"name": "Room E", "capacity": 15},
-        {"name": "Room F", "capacity": 30},
-        {"name": "Room G", "capacity": 15},
-        {"name": "Room H", "capacity": 30},
-        {"name": "Room I", "capacity": 10},
-        {"name": "Room J", "capacity": 30},
+    return [
+        get_building_specs(building_dir)
+        for building_dir in BUILDINGS_DIR.glob("*")
+        if building_dir.is_dir()
     ]
-    return [Room(index=index, **specs) for index, specs in enumerate(room_descriptions)]
+
+
+# def load_adjacency(path: Path) -> NDArray:
+#     """
+#     Loads adjacency matrix from file.
+#     Args:
+#         path: path to the subscription
+#             where the adjacency matrix is.
+
+#     Returns:
+#         a (N_rooms, N_rooms) matrix where 0 represents
+#         two rooms not sharing a wall and 1 represents
+#         said rooms sharing a wall.
+#     """
+#     # TODO: This is a placeholder, the correct implementation
+#     # TODO: must be added here. :)
+
+#     return np.array(
+#         [
+#             [0, 1, 0, 0, 0, 0, 0, 0, 0, 1],
+#             [1, 0, 0, 1, 0, 1, 1, 0, 0, 0],
+#             [0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
+#             [0, 1, 1, 0, 0, 0, 1, 0, 0, 0],
+#             [0, 0, 0, 0, 0, 1, 1, 1, 0, 0],
+#             [0, 1, 0, 0, 1, 0, 0, 0, 0, 0],
+#             [0, 1, 0, 1, 1, 0, 0, 0, 0, 0],
+#             [0, 0, 0, 0, 1, 0, 0, 0, 1, 0],
+#             [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+#             [1, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+#         ]
+#     )
+
+
+# def load_config(path: Path) -> dict[str, Any]:
+#     """
+#     Loads school-specific configuration from a file.
+#     Args:
+#         path: path to the subscription
+#             where the adjacency matrix is.
+
+#     Returns:
+#         The configuration defined by the user.
+#     """
+#     # TODO: This is a placeholder, the correct implementation
+#     # TODO: must be added here. :)
+#     return {
+#         "ranker": "FullRanker",
+#         "costs": {"HeatingCost": {"t_weight": 1.0, "message_importance": 0.5}},
+#     }
+
+
+# def load_room_description(path: Path) -> list[Room]:
+#     """
+#     Loads a description of each room from a file.
+#     Args:
+#         path: path to the subscription where the adjacency matrix is.
+#     Returns:
+#         a list of rooms with the same indexing as the adjacency.
+#     """
+#     # TODO: This is a placeholder, the correct implementation
+#     # TODO: must be added here. :)
+#     room_descriptions = [
+#         {"name": "Room A", "capacity": 30},
+#         {"name": "Room B", "capacity": 20},
+#         {"name": "Room C", "capacity": 10},
+#         {"name": "Room D", "capacity": 30},
+#         {"name": "Room E", "capacity": 15},
+#         {"name": "Room F", "capacity": 30},
+#         {"name": "Room G", "capacity": 15},
+#         {"name": "Room H", "capacity": 30},
+#         {"name": "Room I", "capacity": 10},
+#         {"name": "Room J", "capacity": 30},
+#     ]
+#     return [Room(index=index, **specs)
+#           for index, specs in enumerate(room_descriptions)
+#       ]
