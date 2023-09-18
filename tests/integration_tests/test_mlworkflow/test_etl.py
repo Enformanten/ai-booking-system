2a37fb9fbe7e0e02ee70f3b5332a2734be405b28
Estefania Garijo <estefania.rio@bs.nttdata.com>
2023-09-18 11:33:53 +0200

    Add integration test for binary bookings

--- tests/integration_tests/test_mlworkflow/test_etl.py
+++ tests/integration_tests/test_mlworkflow/test_etl.py
@@ -22,5 +22,15 @@ def test_column_names(raw_dataf: pd.DataFrame, demo_rooms: list[Room]) -> None:
     room_cols = {col for col in raw_dataf.columns if col.startswith("Room")}
 
     # then
-    assert "electricity" in raw_dataf.columns
-    assert len(demo_rooms) == len(room_cols)
+    assert "electricity" in raw_dataf.columns  # there is an electricity col
+    assert len(demo_rooms) == len(room_cols)  # all rooms are present
+
+
+@pytest.mark.ml_integration
+def test_binary_bookings(raw_dataf: pd.DataFrame):
+    # given
+    room_cols = [col for col in raw_dataf.columns if col.startswith("Room")]
+    bookings = raw_dataf[room_cols].to_numpy()
+
+    # then
+    assert ((bookings == 0) | (bookings) == 1).all()  # check it is binary
