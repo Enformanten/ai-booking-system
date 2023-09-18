9bcf23b8a149831eca0034df77c405b57234f49b
Estefania Garijo <estefania.rio@bs.nttdata.com>
2023-09-18 11:28:10 +0200

    Add thest to check columns are right

--- tests/integration_tests/test_mlworkflow/test_etl.py
+++ tests/integration_tests/test_mlworkflow/test_etl.py
@@ -3,11 +3,24 @@ from pathlib import Path
 import pandas as pd
 import pytest
 
+from thermo.utils.room import Room
+
+
+@pytest.fixture
+def raw_dataf(DATADIR: Path) -> pd.DataFrame:
+    yield pd.read_pickle(DATADIR / "raw_data.pkl")
+
 
 @pytest.mark.ml_integration
-def test_datasize(DATADIR: Path) -> None:
+def test_datasize(raw_dataf: pd.DataFrame) -> None:
+    assert raw_dataf.shape == (240, 11)
+
+
+@pytest.mark.ml_integration
+def test_column_names(raw_dataf: pd.DataFrame, demo_rooms: list[Room]) -> None:
     # given
-    dataf = pd.read_pickle(DATADIR / "raw_data.pkl")
+    room_cols = {col for col in raw_dataf.columns if col.startswith("Room")}
 
     # then
-    assert dataf.shape == (240, 11)
+    assert "electricity" in raw_dataf.columns
+    assert len(demo_rooms) == len(room_cols)
